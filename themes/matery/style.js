2d2133827f6b0b309a4beb453c9b88c8ec55ad43
Kuihua222 <153698778+Kuihua222@users.noreply.github.com>
2024-04-06 23:49:21 +0800

    Update style.js

--- themes/matery/style.js
+++ themes/matery/style.js
@@ -1,17 +1,32 @@
 /* eslint-disable react/no-unknown-property */
 /**
- * 此处样式只对当前主题生效
- * 此处不支持tailwindCSS的 @apply 语法
+ * 这里的css样式只对当前主题生效
+ * 主题客制化css
  * @returns
  */
 const Style = () => {
-  return <style jsx global>{`
+  return (<style jsx global>{`
     // 底色
     body{
         background-color: #f5f5f5
     }
     .dark body{
         background-color: black;
+    }
+  
+    /*  菜单下划线动画 */
+    #theme-matery .menu-link {
+        text-decoration: none;
+        background-image: linear-gradient(#928CEE, #928CEE);
+        background-repeat: no-repeat;
+        background-position: bottom center;
+        background-size: 0 2px;
+        transition: background-size 100ms ease-in-out;
+    }
+    
+    #theme-matery .menu-link:hover {
+        background-size: 100% 2px;
+        color: #928CEE;
     }
 
     /* 设置了从上到下的渐变黑色 */
@@ -24,8 +39,38 @@ const Style = () => {
         height: 100%;
         background:  linear-gradient(to bottom, rgba(0,0,0,0.5) 0%, rgba(0,0,0,0.2) 10%, rgba(0,0,0,0) 25%, rgba(0,0,0,0.2) 75%, rgba(0,0,0,0.5) 100%);
     }
+
+    /* Custem */
+    .tk-footer{
+        opacity: 0;
+    }
+
+    // 选中字体颜色
+    ::selection {
+        background: rgba(45, 170, 219, 0.3);
+    }
+
+    // 自定义滚动条
+    ::-webkit-scrollbar {
+        width: 5px;
+        height: 5px;
+    }
+
+    ::-webkit-scrollbar-track {
+        background: transparent;
+    }
+
+    ::-webkit-scrollbar-thumb {
+        background-color: #928CEE;
+    }
+
+    * {
+        scrollbar-width:thin;
+        scrollbar-color: #49b1f5 transparent
+    }
     
-  `}</style>
+
+  `}</style>)
 }
 
 export { Style }
